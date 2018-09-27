95ec209a67ded8dbca3045a312a32e0f7b2a41ff
Nicolas Gordat <nicolas.gordat@some-email-provider.com>
2018-09-27 14:57:42 +0200

    Improvments
    MainWindow is now event based

--- Sii.Net.DGMLTransformer/DGMLTransformer.Presentation/MainWindow.cs
+++ Sii.Net.DGMLTransformer/DGMLTransformer.Presentation/MainWindow.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DGMLTransformer.Presentation.UserControls;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,31 +11,53 @@ using System.Windows.Forms;
 
 namespace DGMLTransformer.Presentation
 {
+    /// <summary>
+    /// MainWindow class.
+    /// Main window of the application.
+    /// </summary>
     public partial class MainWindow : Form
     {
+        /// <summary>
+        /// The <see cref="DgmlSelector"/> user control.
+        /// </summary>
+        private DgmlSelector dgmlSelector { get; set; }
+
+        /// <summary>
+        /// The <see cref="DgmlFilters"/> user control.
+        /// </summary>
+        private DgmlFilters dgmlFilters { get; set; }
+
+        /// <summary>
+        /// The <see cref="DgmlGenerator"/> user control.
+        /// </summary>
+        private DgmlGenerator dgmlGenerator { get; set; }
+
+        /// <summary>
+        /// Initalizes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+
+            this.dgmlSelector = new DgmlSelector();
+            this.dgmlSelector.Dock = DockStyle.Fill;
+            this.dgmlFilters = new DgmlFilters();
+            this.dgmlFilters.Dock = DockStyle.Fill;
+            this.dgmlGenerator = new DgmlGenerator();
+            this.dgmlGenerator.Dock = DockStyle.Fill;
         }
 
+        /// <summary>
+        /// Event receiver for the OnLoad event.
+        /// </summary>
+        /// <param name="e">The event payload.</param>
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
 
-            var DgmlSelectorControl = new UserControls.DgmlSelector();
-            DgmlSelectorControl.Dock = DockStyle.Fill;
-            var DgmlFiltersControl = new UserControls.DgmlFilters();
-            DgmlFiltersControl.Dock = DockStyle.Fill;
-            var DgmlGeneratorControl = new UserControls.DgmlGenerator();
-            DgmlGeneratorControl.Dock = DockStyle.Fill;
-            DgmlSelectorPanel.Controls.Add(DgmlSelectorControl);
-            DgmlFiltersPanel.Controls.Add(DgmlFiltersControl);
-            DgmlGeneratorPanel.Controls.Add(DgmlGeneratorControl);
-        }
-
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
-        {
-
+            this.DgmlSelectorPanel.Controls.Add(this.dgmlSelector);
+            this.DgmlFiltersPanel.Controls.Add(this.dgmlFilters);
+            this.DgmlGeneratorPanel.Controls.Add(this.dgmlGenerator);
         }
     }
 }
